959357b883d4804bfe0eedc60dabf8a0e167ae29
dariofo2 <alejandrodariofo2@gmail.com>
2025-03-19 03:07:16 +0100

    changes

--- frontend/src/components/home.tsx
+++ frontend/src/components/home.tsx
@@ -1,14 +1,15 @@
 "use client"
 import { motion, useScroll, useTransform } from "motion/react"
-import { useRef } from "react"
+import { useRef, useState } from "react"
 
 export default function HHome() {
     const scrollRef=useRef(null);
+    const [itemsLength,useItemsLength]=useState();
     const {scrollYProgress} =useScroll({target:scrollRef});
-    const widthScroll=useTransform(scrollYProgress,[0,1],[0,-500]);
+    const widthScroll=useTransform(scrollYProgress,[0,1],[0,- scrollRef.current?.offsetWidth]);
     return (
         <div>
-            <motion.div ref={scrollRef} className="d-flex column-gap-3" style={{ height: "300vh", x: widthScroll }}  >
+            <motion.div id="container" ref={scrollRef} className="d-flex column-gap-3" style={{ height: "300vh", x:widthScroll}} >
                     <div className="bg-primary position-sticky top-0" style={{ minWidth: 400, height: 400 }}></div>
                     <div className="bg-primary position-sticky top-0" style={{ minWidth: 400, height: 400 }}></div>
                     <div className="bg-primary position-sticky top-0" style={{ minWidth: 400, height: 400 }}></div>
